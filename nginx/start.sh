92f9379e52151568370bc4be5c41ae8e62fa2437
bislogin <137298569+bislogin@users.noreply.github.com>
2026-04-09 12:48:54 +0300

    Update start.sh

--- nginx/start.sh
+++ nginx/start.sh
@@ -9,3 +9,6 @@ git config --global user.name bazhenov
 git config --global user.email bazhenilya@gmail.com
 git branch -M main
 git remote add origin git@github.com:bislogin/otus_test.git
+git pull origin main
+
+cd FE/
