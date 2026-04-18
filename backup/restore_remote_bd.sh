4def25bde721dcb2c9daeb755c49c0201aba8847
bislogin <137298569+bislogin@users.noreply.github.com>
2026-04-18 13:54:30 +0300

    Update restore_remote_bd.sh

--- backup/restore_remote_bd.sh
+++ backup/restore_remote_bd.sh
@@ -9,7 +9,7 @@ DB_NAME="otus"
 
 BACKUP_DIR="/home/bazhenov/backup/mysql/backup/"
 
-LOCAL_BACKUP_PATH=$(ls -t ${BACKUP_DIR}/*[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]*.sql.gz 2>/dev/null | head -n 1)
+LOCAL_BACKUP_PATH=$(find "$BACKUP_ROOT" -name "*.sql.gz" -printf "%T@ %p\n" | sort -rn | head -n 1 | cut -d' ' -f2-)
 
 ssh ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST} "mysql -u ${DB_USER} -p'${DB_PASS}' -e 'CREATE DATABASE IF NOT EXISTS ${DB_NAME};'"
 
