d2c5d205b1365a6e922e1728f629a1500210fe9b
bislogin <137298569+bislogin@users.noreply.github.com>
2026-04-18 17:28:21 +0300

    Update setup.sh

--- monitor/setup.sh
+++ monitor/setup.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+curl -LO https://github.com/prometheus/prometheus/releases/download/v2.46.0/prometheus-2.46.0.linux-amd64.tar.gz
+curl -LO https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
+
 
