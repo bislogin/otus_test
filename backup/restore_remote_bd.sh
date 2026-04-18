55f9284e962dde88f48494f94b5a37d6fbb8e518
bazhenov <bazhenilya@gmail.com>
2026-04-18 14:51:27 +0000

    final

--- backup/restore_remote_bd.sh
+++ backup/restore_remote_bd.sh
@@ -3,17 +3,25 @@
 REMOTE_SSH_USER="bazhenov"
 REMOTE_SSH_HOST="172.20.1.40"
 
-DB_USER="repl"
-DB_PASS="password#2026"
+DB_USER="root"
+#DB_PASS="password#2026"
 DB_NAME="otus"
 
 BACKUP_DIR="/home/bazhenov/backup/mysql/backup"
 
-echo "1."
+echo "1. backup search..."
 LOCAL_BACKUP_PATH=$(find "$BACKUP_DIR" -type f -name "*.sql.gz" -exec ls -t {} + | head -n 1)
 
-echo "2."
-ssh ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST} "mysql -u ${DB_USER} -p'${DB_PASS}' -e 'CREATE DATABASE IF NOT EXISTS ${DB_NAME} CHARACTER SET utf8mb4;'"
+if [ -z "$LOCAL_BACKUP_PATH" ]; then
+    echo "ERROR: File not found. Check the path: ls -l $BACKUP_ROOT"
+    exit 1
+fi
+echo "found: $LOCAL_BACKUP_PATH"
+
+
+
+echo "2. creating a database '$DB_NAME'..."
+ssh ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST} "sudo mysql -u ${DB_USER} -e 'CREATE DATABASE IF NOT EXISTS ${DB_NAME} CHARACTER SET utf8mb4;'"
 
 if [ $? -eq 0 ]; then
     echo "Databases '$DB_NAME' created."
@@ -22,8 +30,8 @@ else
     exit 1
 fi
 
-echo "3."
-gunzip < "$LOCAL_BACKUP_PATH" | ssh ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST} "mysql -u ${DB_USER} -p'${DB_PASS}' ${DB_NAME}"
+echo "3. Starting to import..."
+gunzip < "$LOCAL_BACKUP_PATH" | ssh ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST} "sudo mysql -u ${DB_USER} ${DB_NAME}"
 
 if [ $? -eq 0 ]; then
     echo "Successful recovery!"
