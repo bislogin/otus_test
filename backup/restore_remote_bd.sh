424ef02c4b8de97bc202082e36e7d0922c83e7cc
bislogin <137298569+bislogin@users.noreply.github.com>
2026-04-18 12:35:01 +0300

    Update restore_remote_bd.sh

--- backup/restore_remote_bd.sh
+++ backup/restore_remote_bd.sh
@@ -8,23 +8,20 @@ DB_NAME="otus"
 
 LOCAL_BACKUP_PATH="/home/bazhenov/backup/mysql/test_tbl.sql.gz"
 
-# 1. Создаем базу, если её нет (через SSH)
 ssh ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST} "mysql -u ${DB_USER} -e 'CREATE DATABASE IF NOT EXISTS ${DB_NAME} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;'"
 
 if [ $? -eq 0 ]; then
-    echo "Databases '$DB_NAME' готова (проверена/создана)."
+    echo "Databases '$DB_NAME' created."
 else
-    echo "Ошибка при создании базы данных."
+    echo "Error created, database exists"
     exit 1
 fi
 
-# 2. Распаковываем локальный архив и отправляем поток в MySQL на удаленный сервер
-echo "Загрузка данных в базу..."
-gunzip < "$LOCAL_BACKUP_PATH" | ssh ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST} "mysql -u ${DB_USER} -p'${DB_PASS}' ${DB_NAME}"
+gunzip < "$LOCAL_BACKUP_PATH" | ssh ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST} "mysql -u ${DB_USER} ${DB_NAME}"
 
 if [ $? -eq 0 ]; then
-    echo "Восстановление успешно завершено!"
+    echo "Successful recovery!"
 else
-    echo "Произошла ошибка при импорте данных."
+    echo "Error recovery."
     exit 1
 fi
