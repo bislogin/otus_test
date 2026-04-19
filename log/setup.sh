b9fd6932ebeda981cb7d15f335d8ca11279dcec1
bislogin <137298569+bislogin@users.noreply.github.com>
2026-04-19 20:30:42 +0300

    Update setup.sh

--- log/setup.sh
+++ log/setup.sh
@@ -54,3 +54,75 @@ logging:
       - file
 pid.file: /run/kibana/kibana.pid
 EOF
+
+sudo dpkg -i logstash-8.17.1-amd64.deb
+sudo systemctl enable --now logstash.service
+
+cat <<EOF | sudo tee /etc/logstash/logstash.yml
+path.data: /var/lib/logstash
+path.config: /etc/logstash/conf.d
+path.logs: /var/log/logstash/
+EOF
+
+cat <<EOF | sudo tee /etc/logstash/conf.d/logstash-nginx-es.conf
+input {
+    beats {
+        port => 5400
+    }
+}
+
+filter {
+ grok {
+   match => [ "message" , "%{COMBINEDAPACHELOG}+%{GREEDYDATA:extra_fields}"]
+   overwrite => [ "message" ]
+ }
+ mutate {
+   convert => ["response", "integer"]
+   convert => ["bytes", "integer"]
+   convert => ["responsetime", "float"]
+ }
+ date {
+   match => [ "timestamp" , "dd/MMM/YYYY:HH:mm:ss Z" ]
+   remove_field => [ "timestamp" ]
+ }
+ useragent {
+   source => "agent"
+ }
+}
+
+output {
+ elasticsearch {
+   hosts => ["http://localhost:9200"]
+   #cacert => '/etc/logstash/certs/http_ca.crt'
+   #ssl => true
+   index => "weblogs-%{+YYYY.MM.dd}"
+   document_type => "nginx_logs"
+ }
+ stdout { codec => rubydebug }
+}
+EOF
+
+sudo systemctl restart logstash.service
+
+sudo dpkg -i filebeat-8.17.1-amd64.deb
+
+cat <<EOF | sudo tee /etc/filebeat/filebeat.yml
+filebeat.inputs:
+- type: filestream
+  paths:
+    - /var/log/nginx/*.log
+
+  enabled: true
+  exclude_files: ['.gz$']
+  prospector.scanner.exclude_files: ['.gz$']
+
+filebeat.config.modules:
+  path: ${path.config}/modules.d/*.yml
+  reload.enabled: false
+
+output.logstash:
+  hosts: ["localhost:5400"]
+
+
+EOF
+
