b19493ee4f06d4534bdb459eb3d43e6de2aea932
bislogin <137298569+bislogin@users.noreply.github.com>
2026-04-24 08:53:19 +0300

    Create recovery_be-1.sh

---
+++ backup/recovery_be-1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp /home/bazhenov/log/filebeat_8.17.1_amd64-224190-6bb8de.deb root@172.20.1.20:/home/bazhenov/filebeat_8.17.1_amd64-224190-6bb8de.deb
+scp /home/bazhenov/git/BE-1/recovery.sh root@172.20.1.20:/tmp && ssh root@172.20.1.20 "sudo bash /tmp/recovery.sh"
