ca007858a9e7841c2eb0519a471fcf246fe272c5
bazhenov <bazhenilya@gmail.com>
2026-04-24 05:51:47 +0000

    commit

--- backup/recovery_fe.sh
+++ backup/recovery_fe.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+scp /home/bazhenov/log/filebeat_8.17.1_amd64-224190-6bb8de.deb root@172.20.1.10:/home/bazhenov/filebeat_8.17.1_amd64-224190-6bb8de.deb
 scp /home/bazhenov/git/FE/recovery.sh root@172.20.1.10:/tmp && ssh root@172.20.1.10 "sudo bash /tmp/recovery.sh"
 
